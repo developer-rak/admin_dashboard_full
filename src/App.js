fa324de3fae018ea0351860fa657e4f8c36e3d08
Rashid-Ali-Kalhoro <87558577+Rashid-Ali-Kalhoro@users.noreply.github.com>
2024-04-09 15:01:56 +0500

    user form

--- src/App.js
+++ src/App.js
@@ -6,9 +6,9 @@ import Sidebar from './scenes/global/Sidebar';
 import Dashboard from './scenes/dashboard';
 import Team from './scenes/team';
 import Contacts from './scenes/contacts';
-// import Invoices from './scenes/invoices';
+import Invoices from './scenes/invoices';
+import Form from './scenes/form';
 // import Bar from './scenes/bar';
-// import Form from './scenes/form';
 // import Line from './scenes/line';
 // import Pie from './scenes/pie';
 // import FAQ from './scenes/faq';
@@ -30,9 +30,9 @@ function App() {
               <Route path='/' element={<Dashboard />} />
               <Route path='/team' element={<Team />} />
               <Route path='/contacts' element={<Contacts />} />
-              {/* <Route path='/invoices' element={<Invoices />} /> */}
+              <Route path='/invoices' element={<Invoices />} />
+              <Route path='/form' element={<Form />} />
               {/* <Route path='/bar' element={<Bar />} /> */}
-              {/* <Route path='/form' element={<Form />} /> */}
               {/* <Route path='/line' element={<Line />} /> */}
               {/* <Route path='/pie' element={<Pie />} /> */}
               {/* <Route path='/faq' element={<FAQ />} /> */}
