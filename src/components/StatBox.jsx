07d23f1a35d195f96475b7e2f48a56b0b20baab2
Rashid-Ali-Kalhoro <87558577+Rashid-Ali-Kalhoro@users.noreply.github.com>
2024-04-12 12:52:32 +0500

    dashboard content

--- src/components/StatBox.jsx
+++ src/components/StatBox.jsx
@@ -22,19 +22,19 @@ const StatBox = ({ title, subtitle, icon, progress, increase }) => {
             <Box>
                <ProgressCircle progress={progress} />
             </Box>
+         </Box>
 
-            <Box display="flex" justifyContent="space-between">
-               <Typography variant="h5" sx={{ color: colors.greenAccent[500] }}>
-                  {subtitle}
-               </Typography>
-               <Typography
-                  variant="h5"
-                  fontWeight="italic"
-                  sx={{ color: colors.greenAccent[600] }}
-               >
-                  {increase}
-               </Typography>
-            </Box>
+         <Box display="flex" justifyContent="space-between">
+            <Typography variant="h5" sx={{ color: colors.greenAccent[500] }}>
+               {subtitle}
+            </Typography>
+            <Typography
+               variant="h5"
+               fontWeight="italic"
+               sx={{ color: colors.greenAccent[600] }}
+            >
+               {increase}
+            </Typography>
          </Box>
       </Box>
    );
